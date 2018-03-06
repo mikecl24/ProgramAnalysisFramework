f4fc4e017f158553758b431e9adc60d99dbe8934
Mike Castro Lundin <mcl24.24@gmail.com>
2018-03-06 14:17:28 +0100

    Domain Specification: Comply with element requirements

--- Parser/Domain.fs
+++ Parser/Domain.fs
@@ -1,13 +1,14 @@
 module Domain
 
 //Generated Code Section: Domain type
-// Q -> P( Var * (Q U {?}) * Q )
+// Q -> P( Var * (Q U {QM}) * Q )
 
 // List1 = {QM}
 type List1 =
     | L1_QM
+    | L1_nofrew
 
-// Union1 = Q3 U {QM}
+// Union1 = (Q2 U List1)
 type Union1 = 
     | Q2 of int
     | U1_List1 of List1 // if it is "?"
